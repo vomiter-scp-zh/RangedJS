3c36fdbf140ebda5b627a564af73eed929118016
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2025-07-27 15:49:19 +0800

    0.8.1 hide some methods

--- src/main/java/com/vomiter/rangedjs/item/ArrowShootingProperties.java
+++ src/main/java/com/vomiter/rangedjs/item/ArrowShootingProperties.java
@@ -1,9 +1,15 @@
 package com.vomiter.rangedjs.item;
 
 import com.vomiter.rangedjs.projectile.HitBehavior;
+import dev.latvian.mods.rhino.util.HideFromJS;
 
 public interface ArrowShootingProperties {
+    @HideFromJS
     UseBehavior getUseBehavior();
+
+    @HideFromJS
     Attributes getAttributes();
+
+    @HideFromJS
     HitBehavior getHitBehavior();
 }
